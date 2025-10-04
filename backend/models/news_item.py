f9d5d556747dce12cff4655f37303a4ed3708b6a
t.makeev <t.makeev@edu.centraluniversity.ru>
2025-10-04 18:15:00 +0300

    Бекенд: фиксы схем

--- backend/models/news_item.py
+++ backend/models/news_item.py
@@ -9,12 +9,12 @@ class Source(BaseModel):
 
 
 class NewsItem(BaseModel):
-    id: int | None= Field(..., description="Уникальный идентификатор поста", default=None)
+    id: int | None= Field(description="Уникальный идентификатор поста", default=None)
     title: str = Field(..., description="Заголовок поста")
     content: str = Field(..., description="Основное содержимое поста")
-    tags: list[str]= Field(..., default_factory=list, description="Массив тегов, связанных с постом")
+    tags: list[str]= Field(default_factory=list, description="Массив тегов, связанных с постом")
     createdAt: datetime = Field(..., description="Дата и время создания поста в формате ISO")
     updatedAt: datetime = Field(..., description="Дата и время последнего обновления поста в формате ISO", default=datetime.now())
     hotnessScore: float = Field(..., description="Оценка популярности поста", default=0)
-    isConfirmed: bool | None = Field(..., description="Флаг, подтверждён ли пост официально", default=False)
-    sources: list[Source] = Field(..., default_factory=list, description="Источники информации, использованные для поста")
+    isConfirmed: bool | None = Field(description="Флаг, подтверждён ли пост официально", default=False)
+    sources: list[Source] = Field(default_factory=list, description="Источники информации, использованные для поста")
